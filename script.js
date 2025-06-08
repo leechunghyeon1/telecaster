13b39e36bdb56e3b52a8bb99a0f7657afcaa3fe8
leechunghyeon1 <cndgus0901@naver.com>
2025-06-08 23:33:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function changeBackground(imageFile) {
-  const viewer = document.getElementById('guitarViewer');
-  viewer.setAttribute('environment-image', imageFile);
+  document.body.style.backgroundImage = `url('${imageFile}')`;
   console.log("배경 변경: ", imageFile);
 }
